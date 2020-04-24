3a5bd3e6cb235f2445132babc14aa4ee11820091
Sangsu Lee <physism@korea.ac.kr>
2020-04-24 21:13:18 +0900

    Final version of ellipsoid plot

--- test04_plot_ellipsoid.m
+++ test04_plot_ellipsoid.m
@@ -1,18 +1,24 @@
-Q = diag([4,1,9,-1]);
-Transform1 = [cos(pi/4) -sin(pi/4) 0 0 ; sin(pi/4) cos(pi/4) 0 0 ; 0 0 1 0 ; 0 0 0 1];
-Transform2 = [cos(pi/6) 0 -sin(pi/6) 0 ; 0 1 0 0 ; sin(pi/6) 0 cos(pi/6) 0 ; 0 0 0 1];
-Transform3 = [1 0 0 3 ; 0 1 0 7 ; 0 0 1 5 ; 0 0 0 1];
-Q = Transform1 * Q * Transform1';
-Q = Transform2 * Q * Transform2';
-Q = Transform3 * Q * Transform3';
+% Q = diag([4,1,9,-1]);
+% Transform1 = [cos(pi/4) -sin(pi/4) 0 0 ; sin(pi/4) cos(pi/4) 0 0 ; 0 0 1 0 ; 0 0 0 1];
+% Transform2 = [cos(pi/6) 0 -sin(pi/6) 0 ; 0 1 0 0 ; sin(pi/6) 0 cos(pi/6) 0 ; 0 0 0 1];
+% Transform3 = [1 0 0 3 ; 0 1 0 7 ; 0 0 1 5 ; 0 0 0 1];
+% Q = inv(Transform1)' * Q * inv(Transform1);
+% Q = inv(Transform2)' * Q * inv(Transform2);
+% Q = inv(Transform3)' * Q * inv(Transform3);
 
-% Obtain translation
-trans = -Q(1:3,4);
+Q = [36 0 0 -36 ; 0 9 0 -18 ; 0 0 4 -20 ; -36 -18 -20 136];     % (x-1)^2 + (y-2)^2 / 4 + (z-5)^2 / 9 = 1
 
 % Obtain rotation transform R
-A = Q(1:3,1:3) + trans * trans';
+A = Q(1:3,1:3);
 [V_eig, d_eig] = eig(A, 'vector');      % A = V_eig * diag(d_eig) * V_eig'
-R = inv(V_eig');                        % A = R * diag(d_eig) * R'
+R = inv(V_eig);                         % A = R' * diag(d_eig) * R
+
+% Obtain translation
+trans = (R' * diag(d_eig)) \ Q(1:3,4);
+
+% Normalization
+normalize_factor = trans' * diag(d_eig) * trans - Q(4,4);
+d_eig = d_eig / normalize_factor;
 
 % Axis length
 length_x = nthroot(d_eig(1), -2);
@@ -28,7 +34,7 @@ num_plot = length(x_transformed);
 for i = 1:num_plot
     for j = 1:num_plot
         T = [R trans; zeros(1,3) 1];
-        coord = T * [x_transformed(i,j) y_transformed(i,j) z_transformed(i,j) 1]';
+        coord = T \ [x_transformed(i,j) y_transformed(i,j) z_transformed(i,j) 1]';
         coord = coord / coord(4);  % normalize
         x(i,j) = coord(1);
         y(i,j) = coord(2);
