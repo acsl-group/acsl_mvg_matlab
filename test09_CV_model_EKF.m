39f5dac316548925aa5b4455eaec6c270b27311b
physism <physism@korea.ac.kr>
2020-08-06 15:59:54 +0900

    EKF Ellipsoid construction initial version

--- test09_CV_model_EKF.m
+++ test09_CV_model_EKF.m
@@ -1,11 +1,12 @@
 clear all
 load('test09_data.mat')
+load('bbox.mat')
 
 %% System model
 syms state [20 1] real
-syms dt real
+
 % State equation
-f(state) = [eye(10) dt*eye(10); zeros(10) eye(10)] * state;
+f(state) = [eye(10) eye(10); zeros(10) eye(10)] * state;
 
 % Measurement equation
 Q_star = vech_inverse_sym([eye(10) zeros(10)] * state);
@@ -27,8 +28,45 @@ Jacobian_F = matlabFunction(Jacobian_F);
 h = matlabFunction(h);
 Jacobian_H = matlabFunction(Jacobian_H);
 
-state_init = zeros(20, 1);
-Est1 = EKF(eye(20), eye(20), eye(18), f, Jacobian_F, h, Jacobian_H, state_init);
+x_hat = zeros(20, 1);
+Est1 = EKF(eye(20), eye(20), eye(18), f, Jacobian_F, h, Jacobian_H, x_hat);
+
+%% Define Cameras
+for i = 1:3
+    cam(i) = Camera();
+end
+cam(1).assign_P(P1);
+cam(2).assign_P(P2);
+cam(3).assign_P(P3);
+
+%% Simulation
+num_iteration = size(bbox.cam1, 2);
+num_plot = 30;
+figure();
+for i = 1:num_iteration
+    cam(1).detection(bbox.cam1(:,i));
+    cam(2).detection(bbox.cam2(:,i));
+    cam(3).detection(bbox.cam3(:,i));
+    
+    cam(1).update_c_star();
+    cam(2).update_c_star();
+    cam(3).update_c_star();
+    
+    msr = [cam(1).c_star' cam(2).c_star' cam(3).c_star']';
+    ctrl = [];
+    
+    x_hat = Est1.Estimate(ctrl, msr);
+    Q_star = vech_inverse(x_hat(1:10));
+    try
+        [x,y,z] = ellipsoid_from_Q(Q_star, num_plot);
+        surf(x,y,z); hold on;
+        drawnow;
+    catch
+        
+    end
+end
+
+
 
 
 function output = vech_sym(A)
@@ -57,4 +95,55 @@ function output = vech_inverse_sym(v)
             count = count + 1;
         end
     end
+end
+
+function output = vech_inverse(v)
+    dim = length(v);
+    dim_output = floor(sqrt(dim*2));
+    output = zeros(dim_output);
+    count = 1;
+    for i = 1:dim_output
+        for j = i:dim_output
+            output(j,i) = v(count);
+            output(i,j) = v(count);
+            count = count + 1;
+        end
+    end
+end
+
+function [x, y, z] = ellipsoid_from_Q(Q, num_plot)
+    % Obtain rotation transform R
+    A = Q(1:3,1:3);
+    [V_eig, d_eig] = eig(A, 'vector');      % A = V_eig * diag(d_eig) * V_eig'
+    R = inv(V_eig);                         % A = R' * diag(d_eig) * R
+
+    % Obtain translation
+    trans = (R' * diag(d_eig)) \ Q(1:3,4);
+
+    % Normalization
+    normalize_factor = trans' * diag(d_eig) * trans - Q(4,4);
+    d_eig = d_eig / normalize_factor;
+
+    % Axis length
+    length_x = nthroot(d_eig(1), -2);
+    length_y = nthroot(d_eig(2), -2);
+    length_z = nthroot(d_eig(3), -2);
+
+    [x_transformed,y_transformed,z_transformed] = ellipsoid(0,0,0,length_x,length_y,length_z,num_plot);
+    x = zeros(size(x_transformed));
+    y = zeros(size(y_transformed));
+    z = zeros(size(z_transformed));
+
+    num_plot = length(x_transformed);
+    for i = 1:num_plot
+        for j = 1:num_plot
+            T = [R trans; zeros(1,3) 1];
+            coord = T \ [x_transformed(i,j) y_transformed(i,j) z_transformed(i,j) 1]';
+            coord = coord / coord(4);  % normalize
+            x(i,j) = coord(1);
+            y(i,j) = coord(2);
+            z(i,j) = coord(3);
+        end
+    end
+
 end
